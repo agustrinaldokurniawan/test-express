bc49c801d06c18102ea93f1bd30fa52322fe50b2
agustrinaldokurniawan <agustkurniawan010899@gmail.com>
2023-01-09 21:03:05 +0700

    test

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-// const Auth = require("../controllers/Auth");
-
 router.post("/login", (req, res) => {
   res.send("Hey this is my API running 🥳");
 });
